05a84e4aa9e257906be81ab2acd504fde932e22b
Minecraft224 <83649081+Minecraft224@users.noreply.github.com>
2021-09-28 16:48:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 var mouseEvent = "empty";
-var last_position_of_x, last_position_of_y;
 
     canvas = document.getElementById('myCanvas');
     ctx = canvas.getContext("2d");
@@ -28,19 +27,9 @@ var last_position_of_x, last_position_of_y;
         ctx.beginPath();
         ctx.strokeStyle = color;
         ctx.lineWidth = width_of_line;
-
-        console.log("Last position of x and y coordinates = ");
-        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
-        ctx.moveTo(last_position_of_x, last_position_of_y);
-
-        console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius, 0, 2*Math.PI)
         ctx.stroke();
         }
-
-        last_position_of_x = current_position_of_mouse_x; 
-        last_position_of_y = current_position_of_mouse_y;
     }
 
     canvas.addEventListener("mouseup", my_mouseup);
